b9e85467e217a3d2b2afb66691ff9d7a1e8ac828
shortcircuited <gideon.antonowicz@gmail.com>
2025-08-13 11:51:06 -0400

    Initial shell commit

---
+++ RVIZ_ONE_CLICK_INSTALLER.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+$username=whoami
+sudo apt update
+
+#Change repo to the name of the ros depository you want to install
+$repo=ros-noetic-desktop-full
+$name=noetic
+
+apt list $repo | grep installed
+
+if [[ $? != 0 ]]; then
+	echo -e "\e[36mPackage $repo not installed. Setting up sources.list...\e[0m"
+	sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+	echo -e "\e[36mSetting up keys and installing curl...\e[0m"
+	apt install -y curl
+	curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+	echo -e "\e[36mInstalling $repo...\e[0m"
+	apt install -y $repo
+	echo -e "\e[36mDone\e[0m"
+fi
+
+grep "source /opt/ros/$name/setup.bash" ~/.bashrc >/dev/null || echo "source /opt/ros/$name/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+echo -e "\e[36mWould you like to install extra dependencies for creating and managing your own ROS workspaces? [Y/N] \e[0m"
+read -n 1 ans
+case "$ans" in
+	[yY])
+		echo -e "Installing extra packages..."
+		apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential 
+                ;;
+        *)
+                echo -e "\nSkipping extra package install. Installing only rosdep."
+                apt install -y python3-rosdep
+                ;;
+esac
+
+echo -e "\e[36mInstalling and updating rosdep...\e[0m"
+rosdep init
+rosdep update
+
+echo -e "\n\e[1;33mINITIAL SETUP COMPLETE. INSTALLING RVIZ...\e[0m"
+mkdir -p ./catkin_ws/src
+cd ./catkin_ws/src
+
+#-The following will ALMOST CERTAINLY need to be changed to suit needs that differ from the default.-----------------------------
+git clone https://github.com/hello-robot/stretch_ros.git
+cd .. #cd to ./catkin_ws
+apt list ros-$name-navigation | grep installed
+
+if [[ $? != 0 ]]; then
+        echo -e "\e[36mPackage ros-$name-navigation not installed. Installing...\e[0m"
+        apt update
+        apt install -y ros-$name-navigation
+        echo -e "\e[36mDone\e[0m"
+fi
+
+catkin_make
+
+#currently in ./catkin_ws
+
+
+grep "source $PWD/devel/setup.bash" ~/.bashrc >/dev/null || echo "source $PWD/devel/setup.bash" >> ~/.bashrc
+
+#add aliases to ~/.bash_aliases
+cd ..
+grep "alias runrviz='$PWD/rviz_launch.sh'" ~/.bash_aliases >/dev/null || echo "alias runrviz='$PWD/rviz_launch.sh'" >> ~/.bash_aliases
+
+source ~/.bashrc
+echo -e "\n\e[1;33mAll tasks completed. Use runrviz to run RViz.\e[0m"
---
+++ rviz_launch.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if pgrep roscore >/dev/null; then
+	echo "roscore already running."
+else
+	echo "Starting roscore service..."
+	roscore &
+	wait
+fi
+
+ROBOT_HOST=192.168.1.34
+LAPTOP_IP=$(hostname -I | awk '{print $1}')
+PING_COUNT=4
+
+if [[ -z "LAPTOP_IP" ]]; then
+	echo "Could not determine your local IP address."
+	echo "Make sure you're connected to the same network as $ROBOT_HOST."
+	exit 1
+fi
+
+#export ROS_MASTER_URI=$ROBOT_MASTER_URI
+#export ROS_HOSTNAME=$LAPTOP_IP
+
+echo "ROS_MASTER_URI = $ROS_MASTER_URI"
+echo "ROS_HOSTNAME = $ROS_HOSTNAME"
+echo "Pinging robot..."
+#ping -c $PING_COUNT $ROBOT_HOST >/dev/null 2>&1
+while true; do
+	ping -c $PING_COUNT $ROBOT_HOST && echo -e "\e[32mConnected successfully!\e[0m" && break || {
+		echo -e "\e[31mCannot reach host at $ROBOT_HOST after $PING_COUNT attempts."
+		echo -e "Connection may be slow or nonexistent.\e[33m Press C to continue anyway, R to retry connection, and any key to exit.\e[0m"
+		read -n 1 ans
+		#echo -e automatically interprets formatiing like \n
+		case "$ans" in
+			[cC])
+				echo -e "\nProceeding..."
+				break
+				;;
+			[rR])
+				echo -e "\nRetrying..."
+				;;
+			*)
+				echo -e "\n\e[31mProcess terminated. Exiting...\e[0m"
+				exit 1
+				;;
+		esac
+	}
+done
+echo "Starting RViz..."
+while true; do
+	ls --color=auto ~/stretch/catkin_ws/src/stretch_ros
+	read -p "Type in a package to use: " package
+	ls --color=auto $(rospack find $package)/rviz
+	if [[ $? != 0 ]]; then
+		echo -e "\e[31mPackage directory cannot be found or does not exist.\e[0m"
+	else
+		read -p "Choose config file (not including rviz): " config
+		rviz -d $(rospack find $package)/rviz/$config.rviz || echo -e "\e[31mUnable to launch RViz. Is the service running on the robot? Is the package name correct? Is the rviz config correct?\e[0m"
+		break
+	fi
+done
+#rviz -d ~/Documents/stretch_default.rviz || echo -e "\e[31mUnable to launch RViz. Is a service that would broadcast to RViz running on the robot?\e[0m"
